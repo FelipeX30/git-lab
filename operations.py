4af752afe231aa3f5612aa6543f5c33aef622652
Gabriel <Toootiz.killer@gmail.com>
2024-04-18 13:32:07 -0600

    Borrado comentario

--- operations.py
+++ operations.py
@@ -2,7 +2,8 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+print("The addition of", a, "and", b, "is:", a + b)
 
 # Display the subtraction
-
+print("The subtraction of", a, "from", b, "is:", a - b)
 
