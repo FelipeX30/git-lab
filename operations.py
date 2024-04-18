840fd415be0f36d03401b8798337ffbda4d0774d
Nikole Mrs <nikole@Nikoles-MacBook-Pro.local>
2024-04-17 23:26:32 -0600

    operations file modified

--- operations.py
+++ operations.py
@@ -2,7 +2,12 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+addition_result = a + b
+print(f'The addition of {a} and {b} is {addition_result}')
 
 # Display the subtraction
+subtraction_result = a - b
+print(f'The subtraction of {a} and {b} is {subtraction_result}')
 
-# Display multiplication
+#Display the multiplication
+
